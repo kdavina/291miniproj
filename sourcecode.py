d3b842afc2c47224707788c1c17d1025dfc7a135
Amanda Nguyen <amandanguyen@Amandas-MacBook-Pro.local>
2019-11-01 17:18:39 -0600

    updated

--- sourcecode.py
+++ sourcecode.py
@@ -1,11 +1,30 @@
 import sqlite3
+import getpass
+import re
 
 def main():
-    conn = sqlite3.connect(r"c:\Users\pingkam\Documents\University\Fourth_Year_University\CMPUT 291\sqlite\movie.db")
-    conn.row_factory = sqlite3.Row
+    global conn, c 
+    # path = sysargv[1] 
+    # conn = sqlite3.connect(path)
+    conn = sqlite3.connect('./database_test.db')
     c = conn.cursor()
-    conn.close()
+    c.execute('PRAGMA foreign_keys=ON; ')
+    conn.commit()
 
-main()
-
-
+    # LOGIN SCREEN
+    print("Login Here!")
+    username = input("Enter username: ")
+    password = input("Enter Password: ")
+    
+    # re.match is checking if our username and password contains alphabet, numbers and underscores ONLY
+    if re.match("^[A-Za-z0-9_]*$", username) and re.match("^[A-Za-z0-9_]*$", password):
+        c.execute('SELECT uid FROM users WHERE uid=? and pwd=?;', (username, password)) == None
+            
+        
+    else:
+        print("login failed")
+    
+        
+    
+if __name__ == "__main__":
+    main()

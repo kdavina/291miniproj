6de374ffc8387bf56da50452f0a9874db0704120
NanPsamy <33300791+NanPsamy@users.noreply.github.com>
2019-11-02 20:48:55 -0600

    done function 2

--- sourcecode.py
+++ sourcecode.py
@@ -60,8 +60,9 @@ def login():
     
 def one():
     print("You have chosen to register a birth")
-    """
+    
     while True:
+        #check to see if fname length < 12
         fname = input("Please provide a first name: ")
         if fname != '' and fname.isalpha() == True:
             break
@@ -69,6 +70,7 @@ def one():
             print("Incorrect format")
     
     while True:
+        #check to see if fname length < 12
         lname = input("Please provide a last name: ")
         if lname != '' and lname.isalpha() == True:
             break 
@@ -76,6 +78,7 @@ def one():
             print("Incorrect format")
             
     while True:
+        #if F/M not entered, maybe show line saying invalid entry?
         gender = input("Please provide the gender (M/F): ")
         if gender.upper() == 'F' or gender.upper() == 'M':
             break
@@ -92,30 +95,35 @@ def one():
     
     
     while True:
+        #check to see if bplace length < 20
         bplace = input("Please provide a birth place: ")
         if bplace != '' and bplace.isalpha():
             break      
-    """    
+        
     while True:
+        #check to see if mother's fname length < 12
         mot_fname = input("Please provide mother's first name: ")
         if mot_fname != '' and mot_fname.isalpha():
             break  
         
     while True:
+        #check to see if mother's lname length < 12
         mot_lname = input("Please provide mother's last name: ")
         if mot_lname != '' and mot_lname.isalpha():
             break     
-    """   
+      
     while True:
+        #check to see if father's fname length < 12
         fat_fname = input("Please provide father's first name: ")
         if fat_fname != '' and fat_lname.isalpha():
             break     
         
     while True:
+        #check to see if father's lname length < 12
         fat_lname = input("Please provide father's last name: ")
         if fat_lname != '' and fat_lname.isalpha():
             break  
-    """
+    
     if find_parent(mot_fname, mot_lname) == None:
         missing_parent_info(mot_fname, mot_lname)
         
@@ -140,9 +148,6 @@ def missing_parent_info(fname, lname):
         else:
             try:
                 year, month, day = bdate.split('-')
-                # IF I ENTER IN MONTH OR DATE AS A SINGLE DIGIT IT DOESN'T SAVE AS 0X
-                # YEAR CAN BE ENTERED IN AS A SINGLE DIGIT
-                print(year, month, day)
                 datetime.datetime(int(year),int(month),int(day))
                 break
             except ValueError:
@@ -154,6 +159,7 @@ def missing_parent_info(fname, lname):
             bplace = 'NULL'
             break
         else:
+        #check for length of bplace < 20?
             if bplace.isalpha():
                 break      
             else:
@@ -176,6 +182,7 @@ def missing_parent_info(fname, lname):
             phone_number = 'NULL'
             break
         else:
+        #check if length is less than or equal to 12
             if phone_number.isalpha() and len(phone_number) < 12:
                 break      
             else:
@@ -185,10 +192,113 @@ def missing_parent_info(fname, lname):
                   VALUES
                   (?,?,?,?,?,?)''', (fname, lname, bdate, bplace, address, phone_number))
     conn.commit()
+       
+
+    
     
 def two():
-    ## git check
-    pass
+    print("You have chosen to register a marriage")
+    
+    while True:
+        prt1_fname = input("Please provide Partner 1's first name: ")
+        if prt1_fname != '' and len(prt1_fname) <= 12 and prt1_fname.isalpha() == True:
+            break
+        else:
+            print("Incorrect format")
+    
+    while True:
+        prt1_lname = input("Please provide Partner 1's last name: ")
+        if prt1_lname != '' and len(prt1_lname) <= 12 and prt1_lname.isalpha() == True:
+            break 
+        else:
+            print("Incorrect format")
+            
+    while True:
+        prt2_fname = input("Please provide Partner 2's first name: ")
+        if prt2_fname != '' and len(prt2_fname) <= 12 and prt2_fname.isalpha() == True:
+            break
+        else:
+            print("Incorrect format")
+    
+    while True:
+        prt2_lname = input("Please provide Partner 2's last name: ")
+        if prt2_lname != '' and len(prt2_lname) <= 12 and prt2_lname.isalpha() == True:
+            break 
+        else:
+            print("Incorrect format")
+        
+    if find_partner(prt1_fname, prt1_lname) == None:
+        missing_partner_info(prt1_fname, prt1_lname)
+        
+    elif find_partner(prt2_fname, prt2_lname) == None:
+        missing_partner_info(prt2_fname, prt2_lname)
+        
+        
+def find_partner(fname, lname):
+    c.execute('SELECT fname, lname FROM persons WHERE fname =? AND lname=?;', (fname, lname))
+    return c.fetchone()
+    
+
+# we need first name, last name, birth date, birth place, address and phone. for each partner any column other than first and last can be null
+def missing_partner_info(fname, lname):
+    print("There seems to be no record of {} {} in our database".format(fname,lname))
+    print("Please fill out the information down below. \nHit enter if you do not want to fill this out.")  
+    while True:
+        bdate = input("Please provide a birth date (YYYY-MM-DD): ")
+        if bdate == '':
+            bdate =  'NULL'
+            break
+        else:
+            try:
+                year, month, day = bdate.split('-')
+                datetime.datetime(int(year),int(month),int(day))
+                break
+            except ValueError:
+                print("Invalid Date")
+    
+    while True:
+        bplace = input("Please provide a birth place: ")
+        if bplace == '':
+            bplace = 'NULL'
+            break
+        else:
+            if bplace.isalpha() and len(bplace) <= 20:
+                break      
+            else:
+                print("Invalid input")
+                
+    while True:
+        address = input("Please provide an address: ")
+        if address == '':
+            address = 'NULL'
+            break
+        else:
+            if address.isalpha() and len(address) <= 30:
+                break      
+            else:
+                print("Invalid input")        
+                
+    while True:
+        phone_number = input("Please provide a phone number (123-456-7890): ")
+        if phone_number == '':
+            phone_number = 'NULL'
+            break
+        else:
+            #if validNumber(phone_number) == True:
+            if len(phone_number) <= 12 and re.match("^[0-9]{3}-[0-9]{3}-[0-9]{4}$", phone_number):
+                break      
+            else:
+                print("Invalid input")
+                
+                
+    partner_register = 'INSERT INTO persons(fname, lname, bdate, bplace, address, phone) VALUES (?,?,?,?,?,?)'
+    #c.execute(''' INSERT INTO persons(fname, lname, bdate, bplace, address, phone)
+    #              VALUES
+    #              (?,?,?,?,?,?)''', (fname, lname, bdate, bplace, address, phone_number))
+    c.execute(partner_register, (fname, lname, bdate, bplace, address, phone_number))
+    conn.commit()
+
+
 def three():
 ## git check
     pass
